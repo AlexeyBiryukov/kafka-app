83dd30da2001b8bf15fcd9d7d2ac145ca5433075
Администратор <tropicoexodus@gmail.com>
2021-05-31 10:45:28 +0300

    2

--- src/main/java/prj1/SimpleConsumer.java
+++ src/main/java/prj1/SimpleConsumer.java
@@ -10,12 +10,8 @@ import java.util.Properties;
 
 public class SimpleConsumer {
     public static void main(String[] args) throws Exception {
-//        if(args.length == 0){
-//            System.out.println("Enter topic name");
-//            return;
-//        }
 
-        String topicName = "channel";
+        String topicName = "simple-topic";
         Properties props = new Properties();
 
         props.put("bootstrap.servers", "localhost:9092");
@@ -24,9 +20,9 @@ public class SimpleConsumer {
         props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
         props.put("key.deserializer",
-                "org.apache.kafka.common.serializa-tion.StringDeserializer");
+                "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer",
-                "org.apache.kafka.common.serializa-tion.StringDeserializer");
+                "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer
                 <String, String>(props);
 
@@ -39,7 +35,6 @@ public class SimpleConsumer {
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
-
 
                 System.out.printf("offset = %d, key = %s, value = %s\n",
                         record.offset(), record.key(), record.value());
