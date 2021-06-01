0b52b4ee9b4434cf1b839a169574cc944fb6b6ef
Администратор <tropicoexodus@gmail.com>
2021-06-01 16:34:27 +0300

    .

--- src/main/java/prj5/KafkaConstant.java
+++ src/main/java/prj5/KafkaConstant.java
@@ -1,7 +1,5 @@
-package prj5;
-
-public interface KafkaConstant {
-    public static String KAFKA_BROKER = "localhost:9092";
-    public static Long CLIENT_ID = 1;
-
-}
+//package prj5;
+//
+//public interface KafkaConstant {
+//    public static String KAFKA_BROKER = "localhost:9092";
+//}
--- src/main/java/projectGetClass/Get.java
+++ src/main/java/projectGetClass/Get.java
@@ -49,8 +49,11 @@ public class Get {
         Properties properties = new Properties();
 
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, hostName);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,LongSerializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.put("group.id", "test#1");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,LongSerializer.class.getName());
+//        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
 
         try (KafkaConsumer<Long, String> consumer =
                      new KafkaConsumer<Long, String>(properties)) {
@@ -58,7 +61,7 @@ public class Get {
 
             while (true) {
                 ConsumerRecords<Long, String> records = consumer.poll(100);
-
+                int i = 0;
                 for (ConsumerRecord<Long, String> record: records) {
                     long key = record.key();
                     String message = record.value();
