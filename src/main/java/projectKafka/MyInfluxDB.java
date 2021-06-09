9943b401e96f76f2a7e80c09df52bbca8cacd598
Java <AlexeyBiryukov/job4j_elementary>
2021-06-09 14:34:44 +0300

    .

--- src/main/java/projectKafka/MyInfluxDB.java
+++ src/main/java/projectKafka/MyInfluxDB.java
@@ -1,21 +1,26 @@
 package projectKafka;
 
-import com.sun.net.httpserver.HttpServer;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Pong;
-import java.net.InetSocketAddress;
+import org.slf4j.Logger;
 
 public class MyInfluxDB {
-    final String serverUrl = "http://127.0.0.1:8086",  username = "root", password = "root";
-    final InfluxDB influxDB= InfluxDBFactory.connect(serverUrl, username, password);
+    final String serverUrl = "http://localhost:8086", username = "Mydb", password = "java";
+    final InfluxDB influxDB = InfluxDBFactory.connect(serverUrl, username, password);
+    private Logger log;
 
-    public void setInfluxDB() {
+    public static void main(String[] args) {
+        MyInfluxDB myInfluxDB = new MyInfluxDB();
+    }
+    public void MyInfluxDB() {
         Pong response = this.influxDB.ping();
-        if (response.getVersion().equalsIgnoreCase("unknown")) {
-            System.out.println("error pinging server.");
+            if (response.getVersion().equalsIgnoreCase("unknown")) {
+                log.error("Error pinging server.");
             return;
         }
+        influxDB.createRetentionPolicy("defaultPolicy", "Mydb", "30d", 1, true);
         influxDB.setLogLevel(InfluxDB.LogLevel.BASIC);
     }
 }
+
